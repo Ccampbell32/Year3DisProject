df6c473481fdd8dca4cbdd2eeb570fb46398c6eb
Matthew McCorkell <McCorkell-M@ulster.ac.uk>
2025-10-10 14:46:36 +0100

    more lockpick game
    ui - bugged rotate

--- Assets/MattsStuff/Code/LockpickingMiniGame.cs
+++ Assets/MattsStuff/Code/LockpickingMiniGame.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LockpickingMiniGame : MonoBehaviour
 {
 
 
     [SerializeField] private GameObject pick;
-    [SerializeField] private GameObject centreLine;
+    [SerializeField] private Image progressBar;
+    [SerializeField] private Image breakBar;
     [SerializeField][Range(-90, 90)] private float greenSpotAngle;
     [SerializeField][Range(-90, 90)] private float currentAngle;
 
     [SerializeField] private float unlockProgress;
+    [SerializeField] private float breakingProgress;
 
     private float percentageTurn; //0-1
 
@@ -21,12 +24,15 @@ public class LockpickingMiniGame : MonoBehaviour
 
     private void Start()
     {
+        progressBar.fillAmount = 0f;
+        breakBar.fillAmount = 0f;
+
         isPicking = false;
         unlockProgress = 0f;
+        breakingProgress = 0f;
         centreofScreen = Screen.width / 2;
         currentAngle = 0f;
         greenSpotAngle = Random.Range(-90f, 90f);
-        centreLine.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
     }
     
     private void FixedUpdate()
@@ -50,7 +56,7 @@ public class LockpickingMiniGame : MonoBehaviour
             //right side of screen
             else if (screenPosition.x > Screen.width / 2)
             {
-                percentageTurn = Mathf.InverseLerp(centreofScreen, Screen.width, screenPosition.x);
+                percentageTurn = Mathf.InverseLerp(centreofScreen, Screen.width, screenPosition.z);
                 //Debug.Log(percentageTurn);
                 currentAngle = percentageTurn * 90f;
             }
@@ -58,12 +64,12 @@ public class LockpickingMiniGame : MonoBehaviour
             //left side of screen
             else if (screenPosition.x < Screen.width / 2)
             {
-                percentageTurn = Mathf.InverseLerp(centreofScreen, 0, screenPosition.x);
+                percentageTurn = Mathf.InverseLerp(centreofScreen, 0, screenPosition.z);
                 //Debug.Log(percentageTurn);
                 currentAngle = (-percentageTurn * 90f);
             }
 
-            pick.transform.rotation = Quaternion.Euler(0f, 0f, currentAngle);
+            pick.transform.rotation = Quaternion.Euler( pick.transform.rotation.eulerAngles.x,  pick.transform.rotation.eulerAngles.y, currentAngle);
             //Debug.Log(currentAngle);
             #endregion
         }
@@ -72,9 +78,16 @@ public class LockpickingMiniGame : MonoBehaviour
 
         //Debug.Log(pick.transform.rotation.eulerAngles.z);
 
-        if (isPicking && currentAngle >= greenSpotAngle - 10f && currentAngle <= greenSpotAngle + 10f)
+        if (breakingProgress >= 40)
+        {
+            Debug.Log("Broke");
+            //Puzzle Failed
+        }
+
+        else if (isPicking && currentAngle >= greenSpotAngle - 10f && currentAngle <= greenSpotAngle + 10f)
         {
             unlockProgress += 0.5f;
+            progressBar.fillAmount = unlockProgress / 40f;
 
             Debug.Log("Unlocking");
 
@@ -83,16 +96,19 @@ public class LockpickingMiniGame : MonoBehaviour
                 Debug.Log("Unlocked");
                 Debug.Log("Puzzle Complete");
             }
-            centreLine.transform.rotation = Quaternion.Euler(0f, unlockProgress * 2, 0f);
         }
+
         else if (isPicking && (currentAngle < greenSpotAngle - 10f || currentAngle > greenSpotAngle + 10f))
         {
             Debug.Log("Breaking");
+            breakingProgress += 0.5f;
+            breakBar.fillAmount = breakingProgress / 40f;
         }
+
         else
         {
             unlockProgress = 0f;
-            Debug.Log("Locked");         
+            Debug.Log("Locked");
         }
         #endregion
     }

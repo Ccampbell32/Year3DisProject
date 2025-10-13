ada6e9c40671784f45b554f874a25796f36eedc1
Matthew McCorkell <McCorkell-M@ulster.ac.uk>
2025-10-13 13:27:41 +0100

    LockPick Mini Game complete

--- Assets/MattsStuff/Code/LockpickingMiniGame.cs
+++ Assets/MattsStuff/Code/LockpickingMiniGame.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 using UnityEngine.UI;
 
 public class LockpickingMiniGame : MonoBehaviour
@@ -10,16 +12,25 @@ public class LockpickingMiniGame : MonoBehaviour
     [SerializeField] private Image breakBar;
     [SerializeField][Range(-90, 90)] private float greenSpotAngle;
     [SerializeField][Range(-90, 90)] private float currentAngle;
+    [SerializeField]private float percentLeftToGreen;
+    [SerializeField]private float percentRightToGreen;
+    [SerializeField][Range(20, 40)] private float FullIndicatorDistance;
+    [SerializeField] private float UnlockIndicatorDistance;
 
     [SerializeField] private float unlockProgress;
     [SerializeField] private float breakingProgress;
+    [SerializeField] private float breakThreshold;
+    [SerializeField] private float unlockThreshold;
 
     private float percentageTurn; //0-1
 
     private Vector3 screenPosition;
-    private float centreofScreen;
+    private float centreOfScreen;
 
     private bool isPicking;
+
+    [SerializeField] private GameObject WinText;
+    [SerializeField] private GameObject LostText ;
 
 
     private void Start()
@@ -30,9 +41,12 @@ public class LockpickingMiniGame : MonoBehaviour
         isPicking = false;
         unlockProgress = 0f;
         breakingProgress = 0f;
-        centreofScreen = Screen.width / 2;
+        centreOfScreen = Screen.width / 2;
         currentAngle = 0f;
         greenSpotAngle = Random.Range(-90f, 90f);
+
+        WinText.SetActive(false);
+        LostText.SetActive(false);
     }
     
     private void FixedUpdate()
@@ -45,8 +59,6 @@ public class LockpickingMiniGame : MonoBehaviour
 
         if (!isPicking)
         {
-
-
             if (screenPosition.x < 0 || screenPosition.x > Screen.width || screenPosition.y < 0 || screenPosition.y > Screen.height)
             {
                 //Not on screen do nothing
@@ -56,7 +68,7 @@ public class LockpickingMiniGame : MonoBehaviour
             //right side of screen
             else if (screenPosition.x > Screen.width / 2)
             {
-                percentageTurn = Mathf.InverseLerp(centreofScreen, Screen.width, screenPosition.z);
+                percentageTurn = Mathf.InverseLerp(centreOfScreen, Screen.width, screenPosition.x);
                 //Debug.Log(percentageTurn);
                 currentAngle = percentageTurn * 90f;
             }
@@ -64,9 +76,10 @@ public class LockpickingMiniGame : MonoBehaviour
             //left side of screen
             else if (screenPosition.x < Screen.width / 2)
             {
-                percentageTurn = Mathf.InverseLerp(centreofScreen, 0, screenPosition.z);
+                percentageTurn = Mathf.InverseLerp(centreOfScreen, 0, screenPosition.x);
                 //Debug.Log(percentageTurn);
                 currentAngle = (-percentageTurn * 90f);
+
             }
 
             pick.transform.rotation = Quaternion.Euler( pick.transform.rotation.eulerAngles.x,  pick.transform.rotation.eulerAngles.y, currentAngle);
@@ -78,37 +91,75 @@ public class LockpickingMiniGame : MonoBehaviour
 
         //Debug.Log(pick.transform.rotation.eulerAngles.z);
 
-        if (breakingProgress >= 40)
+        if (breakingProgress >= breakThreshold)
         {
             Debug.Log("Broke");
-            //Puzzle Failed
+           LostText.SetActive(true);       
         }
 
-        else if (isPicking && currentAngle >= greenSpotAngle - 10f && currentAngle <= greenSpotAngle + 10f)
+        else if (isPicking && currentAngle >= greenSpotAngle - FullIndicatorDistance && currentAngle <= greenSpotAngle + FullIndicatorDistance)
         {
-            unlockProgress += 0.5f;
-            progressBar.fillAmount = unlockProgress / 40f;
+            
+            percentLeftToGreen = Mathf.InverseLerp(greenSpotAngle - FullIndicatorDistance , greenSpotAngle, currentAngle);
+            percentRightToGreen = Mathf.InverseLerp(greenSpotAngle + FullIndicatorDistance, greenSpotAngle, currentAngle);
 
-            Debug.Log("Unlocking");
+            //Debug.Log("Percent Left to Green: " + percentLeftToGreen);
+            //Debug.Log("Percent Right to Green: " + percentRightToGreen);
+            //Debug.Log(percentLeftToGreen * 100);
+            //Debug.Log(percentRightToGreen * 100);
 
-            if (unlockProgress >= 40)
+            //Unlocking area
+            if (currentAngle >= greenSpotAngle - UnlockIndicatorDistance)
             {
-                Debug.Log("Unlocked");
-                Debug.Log("Puzzle Complete");
+                unlockProgress += 1f;
+                progressBar.fillAmount = unlockProgress / unlockThreshold;
+                //Debug.Log("Yes");
+
+            }
+            //Left side percent
+            else if (unlockProgress < (percentLeftToGreen * 100f) && percentRightToGreen == 1f)
+            {
+                unlockProgress += 1f;
+                progressBar.fillAmount = unlockProgress / unlockThreshold;
+                //Debug.Log("Unlocking Left");
+            }
+
+            else if (unlockProgress < (percentRightToGreen * 100f) && percentLeftToGreen == 1f)
+            {
+                unlockProgress += 1f;
+                progressBar.fillAmount = unlockProgress / unlockThreshold;
+                //Debug.Log("Unlocking Right");
+
+            }
+            else
+            {
+                //Debug.Log("Breaking");
+                breakingProgress += 0.5f;
+                breakBar.fillAmount = breakingProgress / breakThreshold;
+            }
+
+
+            //Debug.Log("Unlocking");
+
+            if (unlockProgress >= unlockThreshold)
+            {
+                WinText.SetActive(true);
+                //Debug.Log("Puzzle Complete");
             }
         }
 
-        else if (isPicking && (currentAngle < greenSpotAngle - 10f || currentAngle > greenSpotAngle + 10f))
+        else if (isPicking && (currentAngle < greenSpotAngle - FullIndicatorDistance || currentAngle > greenSpotAngle + FullIndicatorDistance))
         {
-            Debug.Log("Breaking");
+            //Debug.Log("Breaking");
             breakingProgress += 0.5f;
-            breakBar.fillAmount = breakingProgress / 40f;
+            breakBar.fillAmount = breakingProgress / breakThreshold;
         }
 
         else
         {
             unlockProgress = 0f;
-            Debug.Log("Locked");
+            progressBar.fillAmount = 0f;
+            //Debug.Log("Locked");
         }
         #endregion
     }
@@ -124,6 +175,18 @@ public class LockpickingMiniGame : MonoBehaviour
         {
             isPicking = false;
             //Debug.Log(isPicking);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            greenSpotAngle = Random.Range(-90f, 90f);
+            //Debug.Log("New Green Spot Angle: " + greenSpotAngle);
+            unlockProgress = 0f;
+            progressBar.fillAmount = 0f;
+            breakingProgress = 0f;
+            breakBar.fillAmount = 0f;
+            WinText.SetActive(false);
+            LostText.SetActive(false);
         }
     }
 }

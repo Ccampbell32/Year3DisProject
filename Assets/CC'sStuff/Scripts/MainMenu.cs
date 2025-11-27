fb0e7e342f4f369aa6dcbfe5e9df4c7fdc9201b0
Ccampbell32 <148860246+Ccampbell32@users.noreply.github.com>
2025-11-27 00:34:07 +0000

    basic main menu added

---
+++ Assets/CC'sStuff/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour
+{
+    public void PlayGame ()
+
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+
+
+
+}
